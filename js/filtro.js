4801a4f0907fbda55cef1e0312bbb9f8cf6672aa
Desktop-Alejo <arodriguez@myksystem.co>
2020-03-16 03:05:24 -0500

    Resposnive design

--- js/filtro.js
+++ js/filtro.js
@@ -1,25 +1,15 @@
 $(function () {
-    // $('.filter').click(function () {
-    //     $(this).addClass('active').siblings().removeClass('active');
-    //     let valor = $(this).attr('data-nombre');
-    //     if (valor == 'todos') {
-    //         $('.cont-work').show('1000');
-    //     } else {
-    //         $('.cont-work').not('.' + valor).hide('1000');
-    //         $('.cont-work').filter('.' + valor).show('1000');
-    //     }
-    // });
 
-    let equipo = $('#equipo').offset().top,
-        servicio = $('#servicio').offset().top,
-        trabajo = $('#trabajo').offset().top,
-        contacto = $('#contacto').offset().top;
+    let about = $('#about').offset().top,
+        services = $('#services').offset().top,
+        clients = $('#clients').offset().top,
+        contact = $('#contact').offset().top;
 
     window.addEventListener('resize', function(){
-        let equipo = $('#equipo').offset().top,
-        servicio = $('#servicio').offset().top,
-        trabajo = $('#trabajo').offset().top,
-        contacto = $('#contacto').offset().top;
+        let about = $('#about').offset().top,
+        services = $('#services').offset().top,
+        clients = $('#clients').offset().top,
+        contact = $('#contact').offset().top;
     });
 
     $('#enlace-inicio').on('click', function(e){
@@ -29,31 +19,31 @@ $(function () {
         },600);
     });
 
-    $('#enlace-equipo').on('click', function(e){
+    $('#enlace-about').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: equipo -100
+            scrollTop: about -100
         },600);
     });
 
-    $('#enlace-servicio').on('click', function(e){
+    $('#enlace-services').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: servicio -100
+            scrollTop: services -100
         },600);
     });
 
-    $('#enlace-trabajo').on('click', function(e){
+    $('#enlace-clients').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: trabajo -100
+            scrollTop: clients -100
         },600);
     });
 
-    $('#enlace-contacto').on('click', function(e){
+    $('#enlace-contact').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: contacto -100
+            scrollTop: contact -100
         },600);
     });
 });
